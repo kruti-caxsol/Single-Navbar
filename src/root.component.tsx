de1cc126c91679f3925b69e81891ebc7e0262232
kruti-caxsol <kruti.paneri@caxsol.com>
2023-09-06 10:24:45 +0530

    initial commit

---
+++ src/root.component.tsx
@@ -0,0 +1,22 @@
+import './Style/style.scss'
+import { Link, BrowserRouter } from 'react-router-dom';
+
+export default function Root(props) {
+  return (
+    <BrowserRouter>
+      <nav>
+        <div className='custom-container'>
+          <ul>
+            <li>
+              <Link to="/planets">Employee</Link>
+            </li>
+            <li>
+              <Link to="/stars">Poetry</Link>
+            </li>
+          </ul>
+        </div>
+      </nav>
+    </BrowserRouter>
+
+  );
+}
